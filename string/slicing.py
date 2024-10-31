ba8839306c980fc634e9b2ef70398ca13c8c72fb
ShubhamKumaR-96 <shubhamkas245@gmail.com>
2024-10-31 18:03:01 +0530

    strings

---
+++ string/slicing.py
@@ -0,0 +1,13 @@
+my_str="my name is shubham"
+print(len(my_str))
+print(my_str[11:18])
+print(my_str[-1:-7:-1])
+print(my_str.capitalize())
+print(my_str.upper())
+# print(str.lower())
+print(my_str.replace("name","start"))
+print(my_str.find("start"))
+print(my_str.count("a"))
+print(my_str.endswith("am"))
+
+
